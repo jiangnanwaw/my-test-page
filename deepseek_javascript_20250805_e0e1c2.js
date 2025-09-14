fc66a63fea9b80167cdc247530c3a3746a95ed83
jiangnanwaw <471010996@qq.com>
2025-09-14 08:36:04 +0800

    Add files via upload

---
+++ deepseek_javascript_20250805_e0e1c2.js
@@ -0,0 +1,4 @@
+const year = document.getElementById('year-select').value;
+const month = document.getElementById('month-select').value;
+const fileName = `${year}${month}.html`;
+const fileUrl = `https://jiangnanwaw.github.io/my-page/${fileName}`;
